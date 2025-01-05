a48c9d297773d72af12038efbf3853b90c8c740a
kbuor <77895173+kbuor@users.noreply.github.com>
2025-01-05 21:02:22 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// Khởi tạo AOS (Animate On Scroll)
+AOS.init({
+  duration: 1000, // Thời gian hiệu ứng
+  easing: 'ease-in-out', // Easing function cho chuyển động mượt mà
+  once: false // Đảm bảo hiệu ứng xuất hiện mỗi khi phần tử cuộn vào màn hình
+});
